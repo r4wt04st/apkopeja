846658cc0f70f25067feea85429ed7e88cc1f916
Eletie <60183758+Eletie@users.noreply.github.com>
2022-01-26 13:47:32 +0000

    pievienoti inputi un def funkcija iesakta

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("es ar tavu mammu")
+vards_uzvards = str(input("Ievadīt vārdu un uzvārdu: "))
+darba_stazs = int(input("Cik ilgi strādā: "))
+alga = int(input("Cik liela alga "))
+
+def alga_menesi(vards_uzvards, darba_stazs, alga):
+  piemaksa = 
